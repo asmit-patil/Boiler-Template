1ecc783a0c595079114837296c20b07175a52d55
asmit-patil <patilasmit0@gmail.com>
2018-06-18 20:21:22 +0530

    boiler template initial commit

--- middlewares/partials.js
+++ middlewares/partials.js
@@ -4,29 +4,30 @@ var config = require('config')
 const Stack = Contentstack.Stack(config.get('apikey'), config.get('accesstoken'), config.get('environment'))
 
 module.exports = function (req, res, next) {
-
+	
+	
 	async.parallel([
 		function (callback) {
 			// Get Header data
 			var Query = Stack.ContentType('header').Query()
-				.toJSON()
-				.find()
-				.spread(function success(result) {
-					callback(null, result[0])
-				}, function error(error) {
-					callback(error)
-				})
+			.toJSON()
+			.find()
+			.spread(function success(result) {
+				callback(null, result[0])
+			}, function error(error) {
+				callback(error)
+			})
 		},
 		function (callback) {
 			// Get Footer data
 			var Query = Stack.ContentType('footer').Query()
-				.toJSON()
-				.find()
-				.spread(function success(result) {
-					callback(null, result[0])
-				}, function error(error) {
-					callback(error)
-				})
+			.toJSON()
+			.find()
+			.spread(function success(result) {
+				callback(null, result[0])
+			}, function error(error) {
+				callback(error)
+			})
 		}], function (error, success) {
 			if (error) return next(error)
 			
@@ -37,4 +38,4 @@ module.exports = function (req, res, next) {
 			
 			next()
 		})
-};
+}
--- routes/home.js
+++ routes/home.js
@@ -1,20 +1,21 @@
 var express = require('express')
 var router = express.Router()
 var Stack = require('../models/contentstack')
-var ContentTypeUID = "home" //unique id of your content type
+
 
 router.get('/', function (req, res) {
-    Stack.ContentType(ContentTypeUID).Query()
+    var contentTypeUID = "<<content_type_UID>>" 
+    
+    Stack.ContentType(contentTypeUID).Query()
         .toJSON()
         .find()
         .spread(function success(result) {
-            console.log(result[0])
-            res.render('home', {   //your view name
+            res.render('<<view_name>>', {   
                 entry: result[0],
             })
         }, function error(error) {
             next(error)
-})
+    })
 })
 
 module.exports = router
