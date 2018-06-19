b2038443256eee681514a2da2f222d80d3abe204
asmit-patil <patilasmit0@gmail.com>
2018-06-19 16:29:31 +0530

    making changes in partials

--- middlewares/partials.js
+++ middlewares/partials.js
@@ -4,38 +4,27 @@ var config = require('config')
 const Stack = Contentstack.Stack(config.get('apikey'), config.get('accesstoken'), config.get('environment'))
 
 module.exports = function (req, res, next) {
+	var contentTypes = ["header", "footer"]
 	
-	
-	async.parallel([
-		function (callback) {
-			// Get Header data
-			var Query = Stack.ContentType('header').Query()
-			.toJSON()
-			.find()
-			.spread(function success(result) {
-				callback(null, result[0])
-			}, function error(error) {
-				callback(error)
-			})
+	async.map(
+		contentTypes,
+		function (contentType, callback) {
+					Stack.ContentType(contentType).Query()
+					.toJSON()
+					.find()
+					.spread(function (result){
+						callback(null, result[0])
+					}	
+				)			
 		},
-		function (callback) {
-			// Get Footer data
-			var Query = Stack.ContentType('footer').Query()
-			.toJSON()
-			.find()
-			.spread(function success(result) {
-				callback(null, result[0])
-			}, function error(error) {
-				callback(error)
-			})
-		}], function (error, success) {
+		function (error, success) {
 			if (error) return next(error)
-			
+
 			res.locals.partials = {}
 			res.locals.partials.header = success[0]
 			res.locals.partials.footer = success[1]
-			
-			
+	
 			next()
-		})
+		}
+	)	
 }
