45a4de857038a53143ef3649ae2be200e1228c23
asmit-patil <patilasmit0@gmail.com>
2018-06-20 10:48:35 +0530

    making changes in partials

--- middlewares/partials.js
+++ middlewares/partials.js
@@ -1,30 +1,25 @@
 var async = require('async')
 var Contentstack = require('contentstack')
 var config = require('config')
-const Stack = Contentstack.Stack(config.get('apikey'), config.get('accesstoken'), config.get('environment'))
+const Stack = Contentstack.Stack(config.get('apikey'), config.get('accesstoken'), config.get('enviournment'))
 
 module.exports = function (req, res, next) {
-	var contentTypes = ["header", "footer"]
-	
-	async.map(
-		contentTypes,
-		function (contentType, callback) {
-					Stack.ContentType(contentType).Query()
-					.toJSON()
-					.find()
-					.spread(function (result){
-						callback(null, result[0])
-					}	
-				)			
-		},
-		function (error, success) {
-			if (error) return next(error)
-
-			res.locals.partials = {}
-			res.locals.partials.header = success[0]
-			res.locals.partials.footer = success[1]
-	
-			next()
-		}
-	)	
+		var contentTypes = ["header", "footer"]
+		async.map(
+			contentTypes,
+			function (contentType, callback) {
+				Stack.ContentType(contentType).Query()
+				.toJSON()
+				.find()
+				.spread(function (result){
+					callback(null, result[0])
+				})			
+			},
+			function (error, success) {
+				if (error) return next(error)
+				res.locals.partials = {}
+				contentTypes.forEach((key, result)=> res.locals.partials[key] = success[result])
+				next()
+			}
+		)	
 }
